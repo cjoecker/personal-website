384c351dac7f7ee378718bcf8960d888cd08393c
Christian Jöcker <christian.joecker@maibornwolff.de>
2021-06-27 19:41:55 +0200

    add mouseConstraint and responsiveness

--- src/SkillsChart/SkillsMap.tsx
+++ src/SkillsChart/SkillsMap.tsx
@@ -1,7 +1,8 @@
-import Matter from 'matter-js';
+import Matter, {Composite, Mouse, MouseConstraint} from 'matter-js';
 import React, { useEffect, useRef, useState } from 'react';
 
 import { useEffectUnsafe } from '../unsafeHooks';
+import styled from "styled-components";
 
 interface SkillsMapProps {
   skills: SkillsType[];
@@ -13,6 +14,7 @@ interface SkillsType {
 }
 
 const BOUNCINESS = 0.7;
+const WALLS_THICKNESS = 10;
 
 export default function SkillsMap({ skills }: SkillsMapProps) {
   const boxRef = useRef<any>(null);
@@ -44,30 +46,18 @@ export default function SkillsMap({ skills }: SkillsMapProps) {
       getWorldWalls(boxRef.current.getBoundingClientRect())
     );
 
-    const mouse = Matter.Mouse.create(canvasRef.current);
-    mouse.pixelRatio = 3;
-    const mouseConstraint = Matter.MouseConstraint.create(engine, {
-      mouse: mouse,
-    });
+    const mouse = Mouse.create(render.canvas),
+        mouseConstraint = MouseConstraint.create(engine, {
+          mouse: mouse,
+          constraint: {
+            stiffness: 0.1,
+            render: {
+              visible: false
+            }
+          }
+        });
+
     World.add(engine.world, mouseConstraint);
-
-
-    // const mouse = Matter.Mouse.create(render.canvas),
-    //     mouseConstraint = Matter.MouseConstraint.create(engine, {
-    //       mouse: mouse,
-    //       constraint: {
-    //         stiffness: 0.2,
-    //         render: {
-    //           visible: false
-    //         }
-    //       }
-    //     });
-    //
-    // World.add(engine.world, mouseConstraint);
-    //
-    // Matter.Events.on(mouseConstraint, "mousedown", function() {
-    //   World.add(engine.world, Matter.Bodies.circle(150, 50, 30, { restitution: 0.7 }));
-    // });
 
     Runner.run(engine);
     Render.run(render);
@@ -81,42 +71,70 @@ export default function SkillsMap({ skills }: SkillsMapProps) {
     };
   }, []);
 
-  useEffectUnsafe(() => {
-    if (render && skills && canvasRef.current) {
-      const mouse = Matter.Mouse.create(canvasRef.current);
-      const mouseConstraint = Matter.MouseConstraint.create(render.engine, {
-        mouse: mouse,
-      });
-      Matter.World.add(render.engine.world, mouseConstraint);
-    }
-  }, [canvasRef.current]);
 
   useEffectUnsafe(() => {
     if (constraints) {
       const { width, height } = constraints;
-      // Dynamically update canvas and bounds
+
       render.bounds.max.x = width;
       render.bounds.max.y = height;
       render.options.width = width;
       render.options.height = height;
       render.canvas.width = width;
       render.canvas.height = height;
-      // Dynamically update floor
-      const floor = render.engine.world.bodies[0];
-      // Matter.Body.setPosition(floor, {
-      //   x: width / 2,
-      //   y: height + STATIC_DENSITY / 2,
-      // });
-      // Matter.Body.setVertices(floor, [
-      //   { x: 0, y: height },
-      //   { x: width, y: height },
-      //   { x: width, y: height + STATIC_DENSITY },
-      //   { x: 0, y: height + STATIC_DENSITY },
-      // ]);
+
+      const topWall = render.engine.world.bodies[0];
+      Matter.Body.setPosition(topWall, {
+        x: width / 2,
+        y: -WALLS_THICKNESS / 2,
+      });
+      Matter.Body.setVertices(topWall, [
+        { x: 0, y: 0 },
+        { x: width, y: 0 },
+        { x: width, y: WALLS_THICKNESS },
+        { x: 0, y: WALLS_THICKNESS },
+      ]);
+
+      const rightWall = render.engine.world.bodies[1];
+      Matter.Body.setPosition(rightWall, {
+        x: width + WALLS_THICKNESS / 2,
+        y: height / 2,
+      });
+      Matter.Body.setVertices(rightWall, [
+        { x: 0, y: 0 },
+        { x: WALLS_THICKNESS, y: 0 },
+        { x: WALLS_THICKNESS, y: height },
+        { x: 0, y: height },
+      ]);
+
+      const bottomWall = render.engine.world.bodies[2];
+      Matter.Body.setPosition(bottomWall, {
+        x: width / 2,
+        y: height + WALLS_THICKNESS / 2,
+      });
+      Matter.Body.setVertices(bottomWall, [
+        { x: 0, y: 0 },
+        { x: width, y: 0 },
+        { x: width, y: WALLS_THICKNESS },
+        { x: 0, y: WALLS_THICKNESS },
+      ]);
+
+      const leftWall = render.engine.world.bodies[3];
+      Matter.Body.setPosition(leftWall, {
+        x: -WALLS_THICKNESS / 2,
+        y: height / 2,
+      });
+      Matter.Body.setVertices(leftWall, [
+        { x: 0, y: 0 },
+        { x: WALLS_THICKNESS, y: 0 },
+        { x: WALLS_THICKNESS, y: height },
+        { x: 0, y: height },
+      ]);
+
     }
   }, [render, constraints]);
   useEffectUnsafe(() => {
-    const CIRCLES_SIZE = 5;
+    const CIRCLES_SIZE = 10;
     const timeouts: Array<NodeJS.Timeout> = [];
     if (render && skills) {
       skills.forEach(skill => {
@@ -128,10 +146,20 @@ export default function SkillsMap({ skills }: SkillsMapProps) {
               render.engine.world,
               Matter.Bodies.circle(
                 randomX,
-                -skill.level * CIRCLES_SIZE,
+                skill.level * CIRCLES_SIZE,
                 skill.level * CIRCLES_SIZE,
                 {
                   restitution: BOUNCINESS,
+                  render: {
+                    sprite: {
+                      texture: createImage(
+                        skill.skillName,
+                        skill.level * CIRCLES_SIZE
+                      ),
+                      xScale: 1,
+                      yScale: 1,
+                    },
+                  },
                 }
               )
             );
@@ -139,6 +167,7 @@ export default function SkillsMap({ skills }: SkillsMapProps) {
         );
       });
     }
+
 
     return () => {
       if (timeouts) {
@@ -148,35 +177,22 @@ export default function SkillsMap({ skills }: SkillsMapProps) {
   }, [skills, render]);
 
   return (
-    <div
-      style={{
-        position: 'relative',
-        border: '1px solid white',
-        padding: '8px',
-        height: '500px',
-        width: '500px'
-      }}
-    >
-      <div
+
+      <CanvasWrapper
         ref={boxRef}
         style={{
-          position: 'absolute',
-          top: 0,
-          left: 0,
           width: '100%',
           height: '100%',
-          pointerEvents: 'none',
         }}
       >
         <canvas ref={canvasRef} />
-      </div>
-    </div>
+      </CanvasWrapper>
+
   );
 }
 
 function getWorldWalls(constraints: any) {
   const { width, height } = constraints;
-  const wallThickness = 1;
 
   const options = {
     isStatic: true,
@@ -185,32 +201,65 @@ function getWorldWalls(constraints: any) {
     },
   };
 
-  const leftWall = Matter.Bodies.rectangle(
-    -wallThickness / 2,
-    height / 2,
-    wallThickness,
-    height,
+  const topWall = Matter.Bodies.rectangle(
+    width / 2,
+    -WALLS_THICKNESS / 2,
+    width,
+    WALLS_THICKNESS,
     options
   );
   const rightWall = Matter.Bodies.rectangle(
-    width + wallThickness / 2,
-    height / 2,
-    wallThickness,
-    height,
-    options
+      width + WALLS_THICKNESS / 2,
+      height / 2,
+      WALLS_THICKNESS,
+      height,
+      options
   );
-  const floor = Matter.Bodies.rectangle(
-    width / 2,
-    height + wallThickness / 2,
-    width,
-    wallThickness,
-    options
+  const bottomWall = Matter.Bodies.rectangle(
+      width / 2,
+      height + WALLS_THICKNESS / 2,
+      width,
+      WALLS_THICKNESS,
+      options
   );
-
+  const leftWall = Matter.Bodies.rectangle(
+      -WALLS_THICKNESS / 2,
+      height / 2,
+      WALLS_THICKNESS,
+      height,
+      options
+  );
   return [
-    // Matter.Bodies.rectangle(width/2, 0, width, 50, { isStatic: true }),
-    floor,
+    topWall,
     rightWall,
+    bottomWall,
     leftWall,
   ];
 }
+
+function createImage($string: string, size: number) {
+  const drawing = document.createElement('canvas');
+  drawing.width = size * 2;
+  drawing.height = size * 2;
+  const ctx = drawing.getContext('2d');
+
+  if (ctx) {
+    ctx.beginPath();
+    ctx.arc(size, size, size, 0, Math.PI * 2);
+    ctx.fillStyle = 'blue';
+    ctx.fill();
+    ctx.closePath();
+
+    ctx.fillStyle = '#fff';
+    ctx.font = '12pt Yantramanav';
+    ctx.textAlign = 'center';
+    ctx.fillText($string, size, size + 4);
+  }
+
+  return drawing.toDataURL('image/png', 10);
+}
+
+const CanvasWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+`
